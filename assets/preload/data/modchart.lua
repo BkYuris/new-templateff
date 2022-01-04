166237f324117422ab5d9a39dd76de4d3f28d3e3
BkYuris <96864066+BkYuris@users.noreply.github.com>
2022-01-04 18:24:42 +0800

    Add files via upload

---
+++ assets/preload/data/modchart.lua
@@ -0,0 +1,15 @@
+function onCreated() --- dont delete this file or the game crash ---
+\* makeLuaDemo()
+or el demo:[5]
+playState make text HealthBar
+!Port By B.K [Yuri]!
+end
+
+function Verify{};
+-@B.K [Yuri]
+-Encrypt code : 7233945
+-Ipad
+end
+^
+make Demo Lua Encrypt :
+^
